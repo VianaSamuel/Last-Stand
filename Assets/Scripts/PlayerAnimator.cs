690709379b6b61d494d20fcaf369f73a4a11946e
VithoriaS <69876384+VithoriaS@users.noreply.github.com>
2023-10-05 16:54:30 -0300

    movimentcao

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
-
+    //References
     Animator am;
     PlayerMovement pm;
     SpriteRenderer sr;
 
-    // Start is called before the first frame update
     void Start()
     {
         am = GetComponent<Animator>();
@@ -17,14 +16,29 @@ public class PlayerAnimator : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(pm.moveDir.x != 0 || pm.moveDir.y != 0){
+        if (pm.moveDir.x != 0 || pm.moveDir.y != 0)
+        {
             am.SetBool("Move", true);
+
+            SpriteDirectionChecker();
         }
-        else{
+        else
+        {
             am.SetBool("Move", false);
+        }
+    }
+
+    void SpriteDirectionChecker()
+    {
+        if (pm.moveDir.x < 0)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
         }
     }
 }
